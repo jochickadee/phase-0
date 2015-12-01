832ecbc19b3b00d77609f5cbeab262e09ba1513f
Johanna Lonn <johanna.lonn@gmail.com>
2015-11-30 20:45:00 -0800

    Add exercises to calc.rb

--- week-4/calc.rb
+++ week-4/calc.rb
@@ -1,3 +1,33 @@
+# How many hours in a year?
 puts 24*365
 
+# How many minutes in a decade? 
 puts ((60*24) * 365) *10
+
+puts 'Hello, cruel world!'
+
+puts 'doom ' * 4
+
+puts 42 + 42
+puts '42' + '42'
+puts '42 + 42'
+
+puts 'You\'re a buttface!'
+
+my_string = '..all work and no play makes Jack a dull boy..'
+
+puts my_string
+puts my_string
+
+name = 'John Jacob Jingleheimer Schmidt'
+
+puts 'My name is ' + name +'.'
+puts 'Do people always shout: \'There goes ' + name + '!\' ?' 
+
+composer = 'Steven Stinking Foster'
+
+puts 'Old Suzanna, Camptown Races, you know - ' + composer + '!'
+
+composer = 'Fredrick Fucking Chopin'
+
+puts 'Oh you know.. ' + composer + '.'
