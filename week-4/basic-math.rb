ce585e28f06c7a079527c2563c6f5902c3e9c248
Johanna Lonn <johanna.lonn@gmail.com>
2015-12-02 16:18:21 -0800

    Complete basic_math.

--- week-4/basic-math.rb
+++ week-4/basic-math.rb
@@ -1,11 +1,17 @@
 # Solution Below
+
 num1 = 13
 num2 = 42
 
+sum = num1 + num2
 
+difference = num1 - num2
 
+product = num1 * num2
 
+quotient = num1.to_f / num2.to_f
 
+modulus = num1.to_f % num2.to_f
 
 # RSpec Tests. They are included in this file because the local variables you are creating are not accessible across files. If we try to run these files as a separate file per normal operation, the local variable checks will return nil.
 
